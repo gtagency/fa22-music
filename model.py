7eaff06fd362748c8792efb16b7ac3210c702af5
Adrian Cheung <adrian.p.cheung@gmail.com>
2022-10-04 19:32:22 -0400

    add model.py, train.py

---
+++ model.py
@@ -0,0 +1,58 @@
+import collections
+import datetime
+import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+import tensorflow as tf
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
+
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Sampling rate for audio playback
+_SAMPLING_RATE = 16000
+
+
+def mse_with_positive_pressure(y_true: tf.Tensor, y_pred: tf.Tensor):
+    mse = (y_true - y_pred) ** 2
+    positive_pressure = 10 * tf.maximum(-y_pred, 0.0)
+    return tf.reduce_mean(mse + positive_pressure)
+
+
+def create_model():
+    input_shape = (seq_length, 3)
+    learning_rate = 0.005
+
+    inputs = tf.keras.Input(input_shape)
+    x = tf.keras.layers.LSTM(128)(inputs)
+
+    outputs = {
+        'pitch': tf.keras.layers.Dense(128, name='pitch')(x),
+        'step': tf.keras.layers.Dense(1, name='step')(x),
+        'duration': tf.keras.layers.Dense(1, name='duration')(x),
+    }
+
+    model = tf.keras.Model(inputs, outputs)
+
+    loss = {
+        'pitch': tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True),
+        'step': mse_with_positive_pressure,
+        'duration': mse_with_positive_pressure,
+    }
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    # model.compile(loss=loss, optimizer=optimizer)
+    # model.summary()
+
+    return model, loss, optimizer
+
---
+++ train.py
@@ -0,0 +1,52 @@
+import collections
+import datetime
+import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+import tensorflow as tf
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
+
+def train(model, loss, optimizer):
+    model.compile(
+        loss=loss,
+        loss_weights={
+            'pitch': 0.05,
+            'step': 1.0,
+            'duration': 1.0,
+        },
+        optimizer=optimizer,
+    )
+
+    model.evaluate(train_ds, return_dict=True)
+
+    callbacks = [
+        tf.keras.callbacks.ModelCheckpoint(
+            filepath='./training_checkpoints/ckpt_{epoch}',
+            save_weights_only=True),
+        tf.keras.callbacks.EarlyStopping(
+            monitor='loss',
+            patience=5,
+            verbose=1,
+            restore_best_weights=True),
+    ]
+
+    epochs = 50
+
+    history = model.fit(
+        train_ds,
+        epochs=epochs,
+        callbacks=callbacks,
+    )
+
+    return history
+
+def plot_history(history):
+    plt.plot(history.epoch, history.history['loss'], label='total loss')
+    plt.show()
