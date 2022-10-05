2e91be89ad694218cfdb78c1df382a6808ee7d40
sriharshak27 <sriharshakocherla27@gmail.com>
2022-10-05 19:51:28 -0400

    Lofi Dataset Processing

---
+++ lofi/ProcessLofiData.py
@@ -0,0 +1,106 @@
+import collections
+import datetime
+import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+import tensorflow as tf
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
+
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Sampling rate for audio playback
+_SAMPLING_RATE = 16000
+
+files = glob.glob("data/archive/*.mid")
+numFiles = len(files)
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+num_files = len(files)
+all_notes = []
+for f in files[:num_files]:
+  notes = midi_to_notes(f)
+  all_notes.append(notes)
+
+all_notes = pd.concat(all_notes)
+
+n_notes = len(all_notes)
+print('Number of notes parsed:', n_notes)
+
+key_order = ['pitch', 'step', 'duration']
+train_notes = np.stack([all_notes[key] for key in key_order], axis=1)
+
+notes_ds = tf.data.Dataset.from_tensor_slices(train_notes)
+notes_ds.element_spec
+
+def create_sequences(
+    dataset: tf.data.Dataset, 
+    seq_length: int,
+    vocab_size = 128,
+) -> tf.data.Dataset:
+  """Returns TF Dataset of sequence and label examples."""
+  seq_length = seq_length+1
+
+  # Take 1 extra for the labels
+  windows = dataset.window(seq_length, shift=1, stride=1,
+                              drop_remainder=True)
+
+  # `flat_map` flattens the" dataset of datasets" into a dataset of tensors
+  flatten = lambda x: x.batch(seq_length, drop_remainder=True)
+  sequences = windows.flat_map(flatten)
+  
+  # Normalize note pitch
+  def scale_pitch(x):
+    x = x/[vocab_size,1.0,1.0]
+    return x
+
+  # Split the labels
+  def split_labels(sequences):
+    inputs = sequences[:-1]
+    labels_dense = sequences[-1]
+    labels = {key:labels_dense[i] for i,key in enumerate(key_order)}
+
+    return scale_pitch(inputs), labels
+
+  return sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
+
+seq_length = 25
+vocab_size = 128
+seq_ds = create_sequences(notes_ds, seq_length, vocab_size)
+seq_ds.element_spec
+
+batch_size = 64
+buffer_size = n_notes - seq_length  # the number of items in the dataset
+train_ds = (seq_ds
+            .shuffle(buffer_size)
+            .batch(batch_size, drop_remainder=True)
+            .cache()
+            .prefetch(tf.data.experimental.AUTOTUNE))
